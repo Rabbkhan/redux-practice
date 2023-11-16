595fd1821f2efee8a2a881eec4fc7df607adf6c1
Rabbilkhan <rabbilkhan125@gmail.com>
2023-11-16 15:06:35 +0530

    Cart code use Redux

---
+++ src/components/Cart/CartButton.js
@@ -0,0 +1,17 @@
+import { useDispatch } from 'react-redux';
+import classes from './CartButton.module.css';
+import { uiActions } from '../../store/ui-slice';
+const CartButton = (props) => {
+const dispatch = useDispatch();
+  const toogleCartHandler =()=>{
+  dispatch(uiActions.toggle());
+  }
+  return (
+    <button className={classes.button} onClick={toogleCartHandler}>
+      <span>My Cart</span>
+      <span className={classes.badge}>1</span>
+    </button>
+  );
+};
+
+export default CartButton;
---
+++ src/store/index.js
@@ -0,0 +1,8 @@
+import {configureStore} from '@reduxjs/toolkit';
+import uiSlice from './ui-slice'
+
+const Store =configureStore({
+reducer:{ui: uiSlice.reducer}
+})
+
+export default Store;
